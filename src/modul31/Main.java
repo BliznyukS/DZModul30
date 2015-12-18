844fa8df6480be17ced5881d7a8843f19070a109
BliznyukS <bliznyuk.sergey@gmail.com>
2015-12-18 18:58:27 +0200

    DZ Modul 3 Test 3

---
+++ src/modul31/Main.java
@@ -0,0 +1,31 @@
+package modul31;
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+// TODO code application logic here
+    }
+
+}
+
+class Shop {
+    MuzInst m1;
+}
+
+class MuzInst {
+
+}
+
+class Guitar extends MuzInst{
+
+}
+
+class Truba extends MuzInst{
+
+}
+
+class Fortepiano extends MuzInst{
+
+}
---
+++ src/modul32/Main.java
@@ -0,0 +1,40 @@
+package modul32;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+// TODO code application logic here
+    }
+
+}
+
+class Buket {
+    Cvetok m1;
+}
+
+class Cvetok {
+
+}
+
+class Roza extends Cvetok{
+
+}
+
+class RozovyiKust {
+    Roza r1;
+}
+
+class Romashka extends Cvetok{
+
+}
+
+class Astra extends Cvetok {
+
+}
+
+class Tulpan extends Cvetok {
+
+}
